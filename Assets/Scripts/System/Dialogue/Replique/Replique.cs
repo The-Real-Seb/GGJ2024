8d4f647b74332755728585bbee6a55f54b4bb6cb
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2024-01-22 21:08:47 +0100

    FirstCommit

---
+++ Assets/Scripts/System/Dialogue/Replique/Replique.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replique : ScriptableObject
+{
+    public string text;
+    public Reponse[] ListReponse;
+}
---
+++ Assets/Scripts/System/Dialogue/Reponse/Reponse.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reponse : ScriptableObject
+{
+    public string afterText;
+    public Image uiImage;
+}
