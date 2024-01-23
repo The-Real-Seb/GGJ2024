2756e5c9b677e82e894e4c0c28abefa1f5520e65
SimplyMarket <louis.bich@gmail.com>
2024-01-23 19:57:14 +0100

    Timer
    Start

---
+++ Assets/Scripts/System/Timer/Timer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private static Timer instance = null;
+    public static Timer Instance => instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
