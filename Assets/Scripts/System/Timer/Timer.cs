3aee27378aba8024a61877698579a2ab0c4e0f35
SimplyMarket <louis.bich@gmail.com>
2024-01-23 21:39:29 +0100

    prout

--- Assets/Scripts/System/Timer/Timer.cs
+++ Assets/Scripts/System/Timer/Timer.cs
@@ -5,7 +5,7 @@ public class Timer : MonoBehaviour
     private static Timer instance = null;
     public static Timer Instance => instance;
 
-
+    private float countdown;
 
     private void Awake()
     {
@@ -18,10 +18,16 @@ public class Timer : MonoBehaviour
         {
             instance = this;
         }
+
+        countdown = DialogueSystem.Instance._replique.repliqueDuration;
     }
 
 
     private void Update()
     {
+        if (countdown > 0)
+            countdown -= Time.deltaTime;
+
+
     }
 }
